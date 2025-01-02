d37f0976ca38ed9bcd35be23393e5c6c17f9da66
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2025-01-02 14:44:03 +0100

    feat: add basic crud methods

--- src/main/java/hutnyk/blog/Repository/ArticleRepository.java
+++ src/main/java/hutnyk/blog/Repository/ArticleRepository.java
@@ -4,6 +4,10 @@ import hutnyk.blog.Model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    List<Article> findByTitle(String title);
 }
--- src/main/java/hutnyk/blog/Service/ArticleService.java
+++ src/main/java/hutnyk/blog/Service/ArticleService.java
@@ -3,11 +3,11 @@ package hutnyk.blog.Service;
 import hutnyk.blog.Mapper.EntityMapper;
 import hutnyk.blog.Model.DTO.ArticleDTO;
 import hutnyk.blog.Repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class ArticleService {
@@ -21,6 +21,30 @@ public class ArticleService {
         this.mapper = mapper;
     }
 
+    public List<ArticleDTO> findAllArticles(){
+        return repository.findAll().stream().map(mapper::articleToDTO).toList();
+    }
 
+    public ArticleDTO findArticleById(Long id){
+        return mapper.articleToDTO(
+                repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Article not found with id: " + id))
+        ) ;
+    }
 
+    public ArticleDTO addArticle(ArticleDTO dto){
+        return mapper.articleToDTO(repository.save(mapper.dtoToArticle(dto)));
+    }
+
+    public List<ArticleDTO> findArticlesByTitle(String title) {
+        return repository.findByTitle(title).stream()
+                .map(mapper::articleToDTO)
+                .toList();
+    }
+
+    public void deleteArticleById(Long id){
+        if(!repository.existsById(id)){
+            throw new EntityNotFoundException("Article not found with id:  + id");
+        }
+        repository.deleteById(id);
+    }
 }
