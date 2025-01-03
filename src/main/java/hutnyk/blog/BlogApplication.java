8c6d3b203b12159f0ec07294e74fc85049c5a28c
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2025-01-03 17:00:22 +0100

    feat: add controller bean

--- src/main/java/hutnyk/blog/BlogApplication.java
+++ src/main/java/hutnyk/blog/BlogApplication.java
@@ -1,5 +1,6 @@
 package hutnyk.blog;
 
+import hutnyk.blog.Controller.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,6 +10,8 @@ public class BlogApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(BlogApplication.class, args);
+		Controller controller = applicationContext.getBean(Controller.class);
+		controller.startApplication();
 	}
 
 }
